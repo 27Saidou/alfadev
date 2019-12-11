dd69eb9e88cd462a22bbcb87153de154af6a7290
Saidou Diallo <sinthio2@gmail.com>
2019-12-11 03:37:35 +0000

    update version

--- version.py
+++ version.py
@@ -1,12 +1,7 @@
 import sqlite3
 
-
-# Create a database in RAM
 db = sqlite3.connect('db_file')
-# Creates or opens a file called mydb with a SQLite3 DB
 cursor = db.cursor()
-
-
 cursor.execute("SELECT * FROM EMP")
 rows = cursor.fetchall()
 
